388f78b48be4432e4d5e3f4f54eb83f3bd5edc6b
Kevin Liu <kevin@kliu.io>
2020-08-31 17:41:53 -0400

    Add replacement functionality

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -1,42 +1,62 @@
 import "webextension-polyfill";
 
-function getPageImages() {
-  const toProcess: { img: HTMLImageElement; originalSrc: string }[] = [];
+const blockedUrl = browser.extension.getURL("block.svg");
 
-  const images = document.body.querySelectorAll("img");
-  for (const img of images) {
-    if (img.src === "" || img.width < 160 || img.height < 160) {
-      continue;
-    }
-    const originalSrc = img.src;
-    toProcess.push({ img, originalSrc });
-  }
-
-  return toProcess;
+function isImageAcceptable(img: HTMLImageElement) {
+  return img.src !== "" && img.width >= 160 && img.height >= 160;
 }
 
-const images = getPageImages();
-console.log("Processing", images);
+function queueImageEvaluation({ src }: HTMLImageElement) {
+  browser.runtime.sendMessage({ src });
+}
 
+const runImagePipeline = (imgs: HTMLImageElement[]) =>
+  imgs.filter(isImageAcceptable).forEach(queueImageEvaluation);
+
+// Process incoming spider detection results
 browser.runtime.onMessage.addListener(
   (
     message: { src: string; err: Error } | { src: string; spiderPct: number }
   ) => {
     if ("err" in message) {
-      console.error(message.err);
+      // L
+      console.error("Image Begone error:", message.err);
     } else {
       const { src, spiderPct } = message;
       console.log(src, ":", spiderPct);
 
-      if (spiderPct > 50) {
-        (document.querySelector(
-          "img[src=" + src + "]"
-        ) as HTMLImageElement).hidden = true;
+      if (spiderPct > 0.5) {
+        const img = Array.from(document.getElementsByTagName("img")).filter(
+          (img) => img.src === src
+        )[0];
+        if (img) {
+          console.log(img);
+          img.src = blockedUrl;
+        } else {
+          console.warn("Image not found");
+        }
       }
     }
   }
 );
 
-for (const image of images) {
-  browser.runtime.sendMessage({ src: image.originalSrc });
-}
+const images = Array.from(document.body.querySelectorAll("img"));
+console.log("Processing", images);
+runImagePipeline(images);
+
+// Watch for new images added to the DOM
+new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    // Filter to only find image elements
+    const images = Array.from(mutation.addedNodes).filter(
+      (el) => el instanceof HTMLImageElement
+    ) as HTMLImageElement[];
+
+    // Run our image pipeline
+    runImagePipeline(images);
+  });
+}).observe(document.body, {
+  attributes: false,
+  childList: true,
+  subtree: true,
+});
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -12,6 +12,20 @@ ReactDOM.render(
     <label>
       <input type="checkbox" checked /> Spiders
     </label>
+
+    <div>
+      Icons made by{" "}
+      <a
+        href="https://www.flaticon.com/authors/dinosoftlabs"
+        title="DinosoftLabs"
+      >
+        DinosoftLabs
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+    </div>
   </div>,
   document.getElementById("root")
 );
